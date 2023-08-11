20a311e0f7f11ac3f5946ef47ea71d130ce051ae
Jezmael Basilio <jezmaelbasilio@gmail.com>
2023-08-11 17:05:22 -0300

    novos arquivos

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Post} from './Post';
+import {Header} from './components/header';
+import './global.css'
+
+export function App(){
+  return (
+    <div>
+      < Header />
+<Post 
+author="Fernanda"
+content="Novo projeto"/>
+</div>
+  )
+}
+
+
